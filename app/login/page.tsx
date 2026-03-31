ee2e2d3d4cc338f3c6974fc1f4e98b57658cb301
imaflukeeee <warakonk.77@gmail.com>
2026-03-31 12:58:03 +0700

    feat: install next.js

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function loginPage() {
+    return (
+        <div className="flex h-screen items-center justify-center bg-gray-100">
+          <div className="bg-white p-8 rounded-lg shadow-md w-96">
+            <h1 className="text-2xl font-bold mb-4">Internal Access</h1>
+            <p className="text-gray-500 mb-6">กรุณาเข้าสู่ระบบ</p>
+            </div>
+        </div>
+    )
+}
